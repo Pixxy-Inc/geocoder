1cc44bef2a34022393428613b150263d7076a33b
eric-fl <eric.otto@frshlns.com>
2018-08-02 14:55:53 -0400

    console log

--- lib/geocoder/results/census.rb
+++ lib/geocoder/results/census.rb
@@ -7,43 +7,43 @@ module Geocoder::Result
       coordinate_data = attributes['coordinates']
       Rails.logger.info "coords ===================================="
       Rails.logger.info coordinate_data.inspect
-      [coordinate_data['y'].to_f, coordinate_data['x'].to_f] 
+      return [coordinate_data['y'].to_f, coordinate_data['x'].to_f] 
     end
 
     def latitude
       Rails.logger.info "lat ===================================="
       Rails.logger.info coordinates[0].inspect
-      coordinates[0]
+      return coordinates[0]
     end
 
     def longitude
       Rails.logger.info "lon ===================================="
       Rails.logger.info coordinates[1].inspect
-      coordinates[1]
+      return coordinates[1]
     end
 
     def address
       Rails.logger.info "matchedAddress ===================================="
       Rails.logger.info attributes['matchedAddress']
-      attributes['matchedAddress']
+      return attributes['matchedAddress']
     end
 
     def street
       Rails.logger.info "street ===================================="
       Rails.logger.info attributes['street']
-      attributes['streetName']
+      return attributes['streetName']
     end
 
     def city
       Rails.logger.info "city ===================================="
       Rails.logger.info attributes['city']
-      attributes['city']
+      return attributes['city']
     end
 
     def state
       Rails.logger.info "state ===================================="
       Rails.logger.info attributes['state']
-      attributes['state']
+      return attributes['state']
     end
 
     alias_method :state_code, :state
@@ -51,15 +51,15 @@ module Geocoder::Result
     def postal_code
       Rails.logger.info "zip ===================================="
       Rails.logger.info attributes['zip']
-      attributes['zip']
+      return attributes['zip']
     end
 
     private
 
     def attributes
       Rails.logger.info "attributes ===================================="
-      Rails.logger.info @data['addressMatches'][0]
-      @data['addressMatches'][0] || {}
+      Rails.logger.info @data['addressMatches'][0].inspect
+      return @data['addressMatches'][0] || {}
     end
     
   end
