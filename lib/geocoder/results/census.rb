c100cd7be13dc4c2df46afa4239883cf093c29df
eric-fl <eric.otto@frshlns.com>
2018-08-02 16:24:52 -0400

    console log

--- lib/geocoder/results/census.rb
+++ lib/geocoder/results/census.rb
@@ -4,15 +4,15 @@ module Geocoder::Result
   class Census < Base
 
     def longitude
-      if @data['coordinates'].empty? 
+      if @data.empty? 
         return null 
       else
         return @data['coordinates']['x']
       end    
     end
-    
+
     def latitude
-      if @data['coordinates'].empty? 
+      if @data.empty? 
         return null 
       else
         return @data['coordinates']['y']
